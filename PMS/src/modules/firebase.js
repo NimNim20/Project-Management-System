fd10f90133b592e4b593eb83b236a23acb1e255c
Simon Nyblom <simnyb01@easv365.dk>
2024-09-24 10:21:42 +0200

    Firebase + Auth

--- PMS/src/modules/firebase.js
+++ PMS/src/modules/firebase.js
@@ -1,3 +1,8 @@
+
+
+import { getAuth } from "firebase/auth";
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,3 +22,7 @@ appId: import.meta.env.VITE_FIREBASE_APP_ID
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
