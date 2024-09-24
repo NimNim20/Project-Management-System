61eae6bb130437f24ae51fdecf76120a294c75a3
NimNim20 <benvio01@easv365.dk>
2024-09-24 11:02:13 +0200

    api fix

--- PMS/src/views/LoginView.vue
+++ PMS/src/views/LoginView.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+// import { ref } from 'vue'
 import { useUsers } from '../modules/useUsers'
 
-const { user, login, logout } = user()
+const { user, login, logout } = useUsers()
 </script>
 
 <template>
