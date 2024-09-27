e20ccb0eb0b46bfe66ece0641a6ed7aceff9b34c
NimNim20 <benvio01@easv365.dk>
2024-09-27 13:23:46 +0200

    Moved some things and made a navbar component

---
+++ PMS/src/components/NavComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
